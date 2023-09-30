c09bbd662e2c80847bfe452863b8be3eb1ad8471
Joseph Lynn <josephlynn94@gmail.com>
2023-09-30 11:38:49 -0400

    wip: dashboard layout with basic components

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import Navigation from "@/components/Navigation";
+import Sidebar from "@/components/Sidebar";
+import Footer from "@/components/Footer";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <main className="flex flex-col min-h-screen">
+      <Navigation />
+      <div className="flex flex-1">
+        <Sidebar />
+        <main className="flex-grow p-4">{children}</main>
+      </div>
+      <Footer />
+    </main>
+  );
+}
